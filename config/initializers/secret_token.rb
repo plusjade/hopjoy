54624733e3dd7f668574f7d3f65a2d3d98f70add
plusjade <plusjade@gmail.com>
2011-09-13 00:48:05 -0700

    new rails 3.1.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hopjoy::Application.config.secret_token = 'ddfbc866799287f504d72afa295c0a88d6b0622b7e5c2cc79f2b78cd6e01a8ece75f3ded3e6500f10ec35ae41e3869e9fb2875fc1f2d5a7e33fb34277522b37c'
