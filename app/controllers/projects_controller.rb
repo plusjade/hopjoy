5a0cc4662e3092f1e54eeb07f2d981887f92f29e
plusjade <plusjade@gmail.com>
2011-09-13 20:59:46 -0700

    add project model and use sti since its a type of page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,7 @@
 class ProjectsController < ApplicationController
   
   def show
-    permalink = params[:permalink].to_s.gsub("-", "_")
-    file = File.new("pages/projects/#{permalink}.md")
-    if file
-      @page = RDiscount.new(file.read).to_html
-    else
-      # throw error
-    end
+    @page = Page.find_by_slug!(params[:slug])
   end
 
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,7 @@ require 'rdiscount'
 class Page < ActiveRecord::Base
 
   def body_to_html
-    RDiscount.new(body).to_html
+    RDiscount.new(body.to_s).to_html
   end
   
 end
