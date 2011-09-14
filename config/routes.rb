7843d7c6e266561a5b0dbd993950c06ac4ff8f36
plusjade <plusjade@gmail.com>
2011-09-13 20:54:54 -0700

    add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Hopjoy::Application.routes.draw do
   root :to => "home#index"
   get "/about" => "home#about"
   get "/projects/:permalink" => "projects#show"
+  get "/pages/:slug" => "pages#show"
 end
