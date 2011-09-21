8c688fae557e6ed850e5f1f1c12e9ed81d337bd1
plusjade <plusjade@gmail.com>
2011-09-21 03:33:12 -0700

    add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Environment.create(
+  [
+    { slug: 'web-browser' }, 
+    { slug: 'web-server' }, 
+    { slug: 'android' }, 
+    { slug: 'ios' }, 
+  ]
+)
+
+Language.create(
+  [
+    { name: 'javascript' }, 
+    { name: 'html' }, 
+    { name: 'css' }, 
+    { name: 'mysql' }, 
+    { name: 'ruby' }, 
+    { name: 'python' }, 
+  ]
+)
+
+p = Project.create({:name => 'tic-tac-toe', :environment_id => 1})
+p.languages << Language.find_by_name("javascript")
+p.languages << Language.find_by_name("html")
+p.languages << Language.find_by_name("css")
+
+browser = Environment.find_by_slug("web-browser")
+browser.languages << Language.find_by_name("javascript")
+browser.languages << Language.find_by_name("html")
+browser.languages << Language.find_by_name("css")
